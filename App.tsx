065c00781f991c935f551090a37e20e85879c10a
endritbeka <endritbeka11@gmail.com>
2021-02-27 20:53:42 +0100

    load fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from "./components/Button"
+import { useFonts } from 'expo-font';
+import fonts from "./constants/fonts"
 
 export default function App() {
+  /* Load fonts */
+  const [loaded] = useFonts({
+    'Product-Sans-Bold': require('./assets/fonts/Product-Sans-Bold.ttf'),
+    'Product-Sans-Italic': require('./assets/fonts/Product-Sans-Italic.ttf'),
+    'Product-Sans-Regular': require('./assets/fonts/Product-Sans-Regular.ttf')
+  });
+
+  if (!loaded) {
+    return null;
+  }
+  
   return (
     <View style={styles.container}>
-      <Button onPress={() => {}} title="B U T T O N" />
+      <Button titleStyle={{ fontFamily: fonts?.regular }} onPress={() => {}} title="B U T T O N" />
     </View>
   );
 }
---
+++ constants/fonts.tsx
@@ -0,0 +1,7 @@
+
+
+export default {
+    regular: 'Product-Sans-Regular',
+    italic: 'Product-Sans-Italic',
+    bold: 'Product-Sans-Bold'
+}
