aa4eb2f2ba078205042b98ea4e1f8572f75eae19
endritbeka <endritbeka11@gmail.com>
2021-02-27 20:12:13 +0100

    Button component addeD

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from "./components/Button"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="B U T T O N" />
     </View>
   );
 }
